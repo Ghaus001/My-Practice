c0e24a7ec60a3a73d9ab5f520c16bf56f5bff5cc
Ghaus001 <g.ghaus001@gmail.com>
2023-11-29 18:31:18 +0500

    Test Commit

--- force-app/main/default/lwc/testBackend/testBackend.js
+++ force-app/main/default/lwc/testBackend/testBackend.js
@@ -1,30 +1,52 @@
 import { LightningElement, track, api } from 'lwc';
 import getContacts from '@salesforce/apex/testBackend.getContacts';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class MyComponent extends LightningElement {
-    contacts;
-    @api recordId = '';
-     firstn = '';
-    lastn = '';
-    handleInputChange(event) {
-        const inputField = event.target;
-        const fieldName = inputField.name;
-        if(fieldName == 'firstName'){
-            this.firstn = inputField.value;
-        }else if(fieldName == 'lastName')
-        this.lastn = inputField.value;
-        console.log(this.firstn + this.lastn); // Add this line
-    }
-    handleClick() {
-        // Make a server request when the button is clicked
-        debugger
-        console.log('acc Id is: '+this.recordId);
-        // You can use the wire adapter or call the Apex method directly
-        getContacts({First:this.firstn, Last:this.lastn, accId:this.recordId})
-       // window.location.reload();
-     }
-    handleClear(){
-        debugger
-         this.firstn = [];
-         this.lastn = [];
-    }
+contacts;
+@api recordId = '';
+firstn = '';
+lastn = '';
+handleInputChange(event) {
+const inputField = event.target;
+const fieldName = inputField.name;
+if(fieldName == 'firstName'){
+    this.firstn = inputField.value;
+}else if(fieldName == 'lastName')
+this.lastn = inputField.value;
+console.log(this.firstn + this.lastn); 
+}
+handleClick() {
+debugger
+console.log('acc Id is: '+this.recordId);
+getContacts({First:this.firstn, Last:this.lastn, accId:this.recordId})
+.then(response => {
+    console.log('Server response:', response);
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Success',
+            message: 'Contact Created Successfully!',
+            variant: 'success',
+        })
+    );
+    setTimeout(() => {
+        window.location.reload();
+    }, 2000);        })
+.catch(error => {
+    this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Error',
+            message: 'Sorry! Contact not Created Successfully!',
+            variant: 'Error',
+        })
+    );
+    setTimeout(() => {
+        window.location.reload();
+    }, 2000);   
+    console.error('Error:', JSON.stringify(error));
+});     }
+handleClear(){
+debugger
+    this.firstn = [];
+    this.lastn = [];
+}
 }
